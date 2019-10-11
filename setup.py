949ff15cee1cffe44852171d981af991eb31f74c
Maxim Litnitskiy <litnimaxster@gmail.com>
2019-10-11 15:09:47 +0300

    Setup added

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='nameko-ari',
+    version='1.0',
+    description='Nameko Asterisk Restfull API (ARI) extension',
+    author='Max Lit',
+    author_email='max.lit.mbox@gmail.com',
+    url='http://github.com/litnimax/nameko-ari',
+    py_modules=['nameko_ari'],
+    install_requires=[
+        "nameko>=2.5.1",
+        "python-ari",
+    ],
+    extras_require={
+        'dev': [
+            "coverage==4.0.3",
+            "flake8==3.3.0",
+            "pylint==1.8.2",
+            "pytest==2.8.3",
+        ]
+    },
+    zip_safe=True,
+    license='Apache License, Version 2.0',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ]
+)
